b9095cb4f5ca27fc64726832e48412d25584afca
tmug94 <44855697+tmug94@users.noreply.github.com>
2019-03-25 16:38:53 -0400

    Update 99 Bottles.py

--- 99 Bottles.py
+++ 99 Bottles.py
@@ -1,8 +1,8 @@
-#Title: 99 bottles
-#Author: Twazanga Mugala
-#Purpose: Create a program that prints out every line to the song "99 bottles of beer on the wall."
+# Title: 99 bottles
+# Author: Twazanga Mugala
+# Purpose: Create a program that prints out every line to the song "99 bottles of beer on the wall."
 
-#Instructions: Do not use a list for all of the numbers, and do not manually type them all in. Use a built in function instead.
+# Requirements: Do not use a list for all of the numbers, and do not manually type them all in. Use a built in function instead.
 #   Besides the phrase "take one down," you may not type in any numbers/names of numbers directly into your song lyrics.
 #   Remember, when you reach 1 bottle left, the word "bottles" becomes singular.
 
